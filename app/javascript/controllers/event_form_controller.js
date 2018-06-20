195905216d41b61c1cf01fa57d20fc20411d336d
Pito Salas <pitosalas@gmail.com>
2018-06-20 09:01:04 -0400

    working example based on Stimulus tutorial part 1

---
+++ app/javascript/controllers/event_form_controller.js
@@ -0,0 +1,13 @@
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+    static targets = ["charcount", "name"]
+    initialize() {
+        this.update()
+    }
+    update() {
+        var countdiv = this.nameTarget.value.length
+        this.charcountTarget.textContent = countdiv
+    }
+}
